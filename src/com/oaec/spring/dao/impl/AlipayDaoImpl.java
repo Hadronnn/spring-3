518c943dfe406cae33f01fd8228b1e9dc26b275e
hadronnn <1364636420@qq.com>
2019-07-08 08:59:28 +0800

    转账功能的事务控制

--- src/com/oaec/spring/dao/impl/AlipayDaoImpl.java
+++ src/com/oaec/spring/dao/impl/AlipayDaoImpl.java
@@ -1,9 +1,13 @@
 package com.oaec.spring.dao.impl;
 
 import com.oaec.spring.dao.AlipayDao;
+import com.oaec.spring.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("alipayDao")
 public class AlipayDaoImpl implements AlipayDao {
@@ -19,7 +23,18 @@ public class AlipayDaoImpl implements AlipayDao {
 
     @Override
     public int sub(String account, Double amount) {
+        Double balance = getBalanceByAccount(account);
+        if (balance < amount){
+            //余额不足
+            throw new RuntimeException("余额不足");
+        }
         String sql = "UPDATE alipay SET balance = balance - ? WHERE account = ?";
         return jdbcTemplate.update(sql,amount,account);
     }
+
+    private Double getBalanceByAccount(String account) {
+        String sql = "SELECT balance FROM alipay WHERE account = ?";
+        return jdbcTemplate.queryForObject(sql,Double.class,account);
+    }
+
 }
--- src/com/oaec/spring/service/impl/AlipayServiceImpl.java
+++ src/com/oaec/spring/service/impl/AlipayServiceImpl.java
@@ -4,15 +4,17 @@ import com.oaec.spring.dao.AlipayDao;
 import com.oaec.spring.service.AlipayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("alipayService")
 public class AlipayServiceImpl implements AlipayService {
     @Autowired
     private AlipayDao alipayDao;
+    @Transactional
     @Override
     public int tranfer(String fromAccount, String toAccount, Double amount) {
-        int sub = alipayDao.sub(fromAccount, amount);
         int add = alipayDao.add(toAccount, amount);
+        int sub = alipayDao.sub(fromAccount, amount);
         return sub+add;
     }
 }
